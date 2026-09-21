b73c25dba27b5e4393bf391471f4803a16341dc9
Ahmed Abdelsalam <A3abdelsalam2005@gmail.com>
2026-09-22 02:26:24 +0300

    Wrote the test bench for the horizontal and vertical counters

---
+++ tb/vga_sync_tb.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module vga_sync_tb;
+
+  reg clk;
+  reg rst;
+  wire hsync;
+  wire vsync;
+  wire video_on;
+  wire [9:0] pixel_x;
+  wire [9:0] pixel_y;
+
+  initial begin
+    clk = 0;
+    rst = 1;
+    #100;
+    rst = 0;
+  end
+
+  always #20 clk = ~clk;
+  
+  vga_sync vga(
+      .clk(clk), 
+      .rst(rst), 
+      .hsync(hsync), 
+      .vsync(vsync), 
+      .video_on(video_on), 
+      .pixel_x(pixel_x), 
+      .pixel_y(pixel_y)
+  );
+
+endmodule
